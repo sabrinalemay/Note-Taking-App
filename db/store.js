769927ba09ce08f09d0997b32ed730ff43d360a9
Sabrina LeMay <b33na1997@gmail.com>
2022-08-21 13:48:38 -0700

    updated the server.js and removed code from api files

--- db/store.js
+++ db/store.js
@@ -1,48 +1,48 @@
-const { json } = require("express");
-const fs = require("fs");
-const util = require("util");
-const uuid = require('uuid');
-uuid.v4();
+// const { json } = require("express");
+// const fs = require("fs");
+// const util = require("util");
+// const uuid = require('uuid');
+// uuid.v4();
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+// const readFileAsync = util.promisify(fs.readFile);
+// const writeFileAsync = util.promisify(fs.writeFile);
 
-class Store {
-    read(){
-        return readFileAsync("db/db.json", "utf8");
-    }
-    write(note){
-        return writeFileAsync("db/db.json", JSON.stringify(note));
-    }
-    getNotes(){
-        return this.read().then((notes) =>{
-            let parsedNotes;
-            try {
-                parsedNotes = [].concat(JSON.parse(notes));
-            }
-            catch(err){
-                parsedNotes = [];
-            }
-            return parsedNotes;
-        });
-    }
-    addNote(note){
-        const { title, text}= note;
-        if (!title || !text) {
-            throw new Error ("note title and text cannot be blank");
-        }
-        const newNote = { title, text, id: uuid()};
-        return this.getNotes()
-        .then((notes) => [...notes, newNote])
-        .then((updatedNotes) => this.write(updatedNotes))
-        .then(() => newNote);
-    }
+// class Store {
+//     read(){
+//         return readFileAsync("db/db.json", "utf8");
+//     }
+//     write(note){
+//         return writeFileAsync("db/db.json", JSON.stringify(note));
+//     }
+//     getNotes(){
+//         return this.read().then((notes) =>{
+//             let parsedNotes;
+//             try {
+//                 parsedNotes = [].concat(JSON.parse(notes));
+//             }
+//             catch(err){
+//                 parsedNotes = [];
+//             }
+//             return parsedNotes;
+//         });
+//     }
+//     addNote(note){
+//         const { title, text}= note;
+//         if (!title || !text) {
+//             throw new Error ("note title and text cannot be blank");
+//         }
+//         const newNote = { title, text, id: uuid()};
+//         return this.getNotes()
+//         .then((notes) => [...notes, newNote])
+//         .then((updatedNotes) => this.write(updatedNotes))
+//         .then(() => newNote);
+//     }
 
-    removeNote(id){
-        return this.getNotes()
-        .then((notes) => notes.filter((note) => note.id !== id))
-        .then((filteredNotes) => this.write(filteredNotes));
-    }
-}
+//     removeNote(id){
+//         return this.getNotes()
+//         .then((notes) => notes.filter((note) => note.id !== id))
+//         .then((filteredNotes) => this.write(filteredNotes));
+//     }
+// }
 
-module.exports = new Store();
+// module.exports = new Store();
--- server.js
+++ server.js
@@ -1,18 +1,80 @@
 const path = require("path");
 const express = require("express");
+const { notes } = require("./db/db.json");
 const fs = require("fs");
-const util = require("util");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+// const util = require("util");
+// const { createInflateRaw } = require("zlib");
+// const apiRoutes = require("./routes/apiRoutes");
+// const htmlRoutes = require("./routes/htmlRoutes");
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+// app.use("/api", apiRoutes);
+// app.use("/", htmlRoutes);
+
+function findAll(query, notesArr) {
+    let filterNotes = notesArr
+    if (query.title){
+        filterNotes = filterNotes.filter(
+            (note) => note.title === query.title,
+        )
+    } return filterNotes
+}
+
+function findById(id, notesArr) {
+    const result = notesArr.filter((notes) => notes.id === id)
+    return result
+}
+
+function createNew(body, notesArr) {
+    const note = body
+    notesArr.push(note)
+    fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify({notes: notesArr}, null, 2)
+    )
+    return note
+} 
+
+app.get("/api/notes",(req, res) => {
+    let results = notes
+    if (req.query) {
+        results = findAll(req.query, results)
+    }
+    res.json(results)
+})
+
+app.get("/api/notes/:id", (req,res) => {
+    const result = findById(req.params.id, notes)
+    if (result) {
+        res.json(result)
+    } else {
+        res.send(404)
+    }
+})
+
+app.post("api/notes", (req,res) => {
+    req.body.id = notes.length.toString()
+    const note = createNew(req.body, notes)
+    res.json(note)
+})
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
+app.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
 
 app.listen(PORT, function(){
     console.log("app listening" + PORT);
